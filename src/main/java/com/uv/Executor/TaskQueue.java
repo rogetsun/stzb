1761db380c4432e49928e8697fa4a06a2bc164cf
rogetsun <songyw163@163.com>
2020-03-16 02:59:43 +0800

    修改线程池逻辑:超过核心线程数时,先加线程再放队列

--- src/main/java/com/uv/Executor/TaskQueue.java
+++ src/main/java/com/uv/Executor/TaskQueue.java
@@ -26,7 +26,7 @@ public class TaskQueue<R extends Runnable> extends LinkedBlockingQueue<R> {
     // offer方法的含义是：将任务提交到队列中，返回值为true/false，分别代表提交成功/提交失败
     @Override
     public boolean offer(R runnable) {
-        log.trace("offer" + runnable);
+        log.trace("offer: " + runnable);
         if (executor == null) {
             log.error("executor == null");
             throw new RejectedExecutionException("The task queue does not have executor!");
@@ -35,27 +35,28 @@ public class TaskQueue<R extends Runnable> extends LinkedBlockingQueue<R> {
         int currentPoolThreadSize = executor.getPoolSize();
         if (executor.getSubmittedTaskCount() < currentPoolThreadSize) {
             // 已提交的任务数量小于当前线程数，意味着线程池中有空闲线程，直接扔进队列里，让线程去处理
-            log.trace("normal");
+            log.trace("poolSize < coreSize");
             return super.offer(runnable);
         }
 
         // return false to let executor create new worker.
         if (currentPoolThreadSize < executor.getMaximumPoolSize()) {
             // 重点： 当前线程数小于 最大线程数 ，返回false，暗含入队失败，让线程池去创建新的线程
-            log.trace("core full");
+            log.trace("coreSize < poolSize < maxSize");
             return false;
         }
 
         // 重点: 代码运行到此处，说明当前线程数 >= 最大线程数，需要真正的提交到队列中
-        log.trace("max full");
+        log.trace("poolSize >= maxSize");
         return super.offer(runnable);
     }
 
-    public boolean retryOffer(R o, long timeout, TimeUnit unit) throws InterruptedException {
+    public boolean retryOffer(R r, long timeout, TimeUnit unit) throws InterruptedException {
+        log.trace("retry offer: " + r);
         if (executor.isShutdown()) {
             throw new RejectedExecutionException("Executor is shutdown!");
         }
-        return super.offer(o, timeout, unit);
+        return super.offer(r, timeout, unit);
     }
 
 }
