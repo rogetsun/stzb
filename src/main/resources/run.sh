1173368a3b2b761b67a5acc4af2808c95a892a9d
rogetsun <songyw163@163.com>
2020-05-20 16:18:03 +0800

    run.sh

--- src/main/resources/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-docker stop stzb
-docker rm stzb
-docker rmi registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
-docker pull registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
-docker run -itd --name stzb -v /home/docker/stzb/config:/config -v /home/docker/stzb/logs:/logs -v /etc/localtime:/etc/localtime -p 12345:8080 --restart=on-failure registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
