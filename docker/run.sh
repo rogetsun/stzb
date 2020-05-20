7dccd99de8754ba67862182d10d293bac565bb8e
rogetsun <songyw163@163.com>
2020-05-20 16:17:38 +0800

    run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-dname="stzb"
+cn="stzb"
 
-docker stop ${dname}
-docker rm ${dname}
+docker stop ${cn}
+docker rm ${cn}
 docker rmi registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
 docker pull registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
-docker run -itd --name ${dname} -v /home/docker/${dname}/logs:/logs -v /etc/localtime:/etc/localtime -p 8080:80 --restart=on-failure registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
+docker run -itd --name ${cn} -v /home/docker/${cn}/config:/config -v /home/docker/${cn}/logs:/logs -v /etc/localtime:/etc/localtime -p 12345:80 \
+--restart=on-failure registry.cn-beijing.aliyuncs.com/songyw/stzb:latest
